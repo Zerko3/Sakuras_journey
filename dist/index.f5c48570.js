e809b21010aafc6672fbd055b6c018034815554c
Zerko3 <zan.zerak99@gmail.com>
2023-05-30 10:15:23 +0200

    working on nav

--- dist/index.f5c48570.js
+++ dist/index.f5c48570.js
@@ -557,6 +557,25 @@ function hmrAccept(bundle, id) {
 }
 
 },{}],"8ZNvh":[function(require,module,exports) {
+"use strict";
+// DOM ELEMENTS
+const hamburgerNav = document.querySelector(".navigation--hamburger");
+const hamburgerSideNavigation = document.querySelector(".navigation--hamburger--box--parent");
+const hamburgerLineOne = document.querySelector(".line--1");
+const hamburgerLineTwo = document.querySelector(".line--2");
+const hamburgerLineThree = document.querySelector(".line--3");
+// Toggle hamburger side navigation view
+const toggleHamburgerNavView = function() {
+    hamburgerSideNavigation.classList.toggle("navigation--hamburger--active");
+    hamburgerLineOne.classList.toggle("line--1--animated");
+    hamburgerLineTwo.classList.toggle("line--2--animated");
+    hamburgerLineThree.classList.toggle("line--3--animated");
+};
+// Initialization
+const init = function() {
+    hamburgerNav.addEventListener("click", toggleHamburgerNavView);
+};
+init();
 
 },{}]},["47Zjz","8ZNvh"], "8ZNvh", "parcelRequire1782")
 
